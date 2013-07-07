92e31feae84af1e9334fedc950255622df887704
Emma Barber <ecbarber1@gmail.com>
2013-07-07 11:09:24 -0400

    Added addons and relevant multipliers

--- developModule.js
+++ developModule.js
@@ -1,6 +1,24 @@
 exports.rand = function modify(min1,max1,min2,max2) {
-
+var addon = require('./addons');
 db = require('./db');
+
+var modules = {
+	chat: new Addon(0, .1, 0, 0),
+	like: new Addon(0, .1, 0, .1),
+	dislike: new Addon(0, .1, 0, .1),
+	video: new Addon(0, .1, 0, 0),
+	interface: new Addon(-.2, .2, -.2, .2),
+	custom: new Addon(0, .1, 0, .1),
+	aprFools: new Addon(0, .1, 0, 0),
+	wait: new Addon(0, .1, 0, .1),
+	games: new Addon(0, .1, 0, .1),
+	paid: newAddon(-.2, .2, 0, .1),
+	face: newAddon(0, .1, 0, .1),
+	wingman: newAddon(0, .1, 0, .1),
+	adsSelect: newAddon(0, .1, 0, .1),
+	adsUp: newAddon(0, .1, 0, .1),
+	adsDown: newAddon(0, .1, 0, .1);
+};
 
 var player = db.getPlayer();
 	
