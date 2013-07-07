d9af8a50c47d97c8ab2e959d67d4611683124a12
Hamzah Khan <khanh@catlin.edu>
2013-07-07 08:05:12 -0700

    Collision Fix

--- purchase.js
+++ purchase.js
@@ -6,7 +6,6 @@ var competitor = require('./competitorGeneration')
 
 // Does db.set modify the player characteristics by default?
 // Does the first element (the property) have to be a string to redefine it?
-<<<<<<< HEAD
 exports.negotiation = function negotiation() {
   if (offer > competitor.worth - tolerance) {
   	db.set(users,(users) + competitor.users);
@@ -15,16 +14,3 @@ exports.negotiation = function negotiation() {
   	db.set(balance, (balance) - offer);
   }
 };
-=======
-
-if (offer > competitor.worth - tolerance) {
-	db.set(player.phoneNumber,users,(users) + competitor.users);
-	db.set(player.phoneNumber,rpu, (rpu)*((users)/(users + competitor.users)) + competitor.rpu*((competitor.users)/(users + competitor.users)));
-	db.set(player.phoneNumber,employees, (emp) + competitor.emp);
-	db.set(player.phoneNumber,balance, (balance) - offer);
-}
-};
-
-
-// +15037296270
->>>>>>> c751373e0e230a50dfaf4b9a48a85ab67f99a492
