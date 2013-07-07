8f98e594c5f89c60d427dac2efdb2f6b1d362842
Emma Barber <ecbarber1@gmail.com>
2013-07-07 00:07:51 -0400

    Modified redis queries

--- command.js
+++ command.js
@@ -1,7 +1,7 @@
 var DEFAULT_USERS = 5;
 var DEFAULT_COMPETITORS = 5;
 var DEFAULT_EMPLOYEES = 5;
-var DEFAULT_FEATURES;
+var DEFAULT_FEATURES = 6;
 var DEFAULT_INVESTORS = 5;
 var DEFAULT_MONEY = 500;
 
@@ -11,18 +11,17 @@ var GAMESTATE_FINISHED = false;
 // Game state functions
 var initGame = function(phoneNumber) {
 	//register user as player in db
-	//HSET uid:phone_number users DEFAULT_USERS
-	client.hset("uid:" + phoneNumber, "users", DEFAULT_USERS);
-	//HSET uid:phone_number gamestate GAMESTATE_STARTED
-	//HSET uid:phone_number competitors DEFAULT_COMPETITORS
-	//HSET uid:phone_number employees DEFAULT_EMPLOYEES
-	//HSET uid:phone_number features DEFAULT_FEATURES
-	//HSET uid:phone_number investors DEFAULT_INVESTORS
-	//HSET uid:phone_number money DEFAULT_MONEY
+	client.set("uid:" + phoneNumber + ":users", DEFAULT_USERS);
+	client.set("uid:" + phoneNumber, + ":gamestate", GAMESTATE_STARTED);
+	client.set("uid:" + phoneNumber + ":competitors", DEFAULT_COMPETITORS);
+	client.set("uid:" + phoneNumber + ":employees", DEFAULT_EMPLOYEES);
+	client.set("uid:" + phoneNumber + ":features", DEFAULT_FEATURES);
+	client.set("uid:" + phoneNumber + ":investors", DEFAULT_INVESTORS);
+	client.set("uid:" + phoneNumber + ":money", DEFAULT_MONEY);
 };
 var ipo = function(phoneNumber) {
 	//remove user from db
-	//HSET uid:phone_number:gamestate GAMESTATE_FINISHED
+	client.set("uid:" + phoneNumber + ":gamestate", GAMESTATE_FINISHED);
 };
 
 // User information
