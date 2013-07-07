f36eee3c7f1a403c9de67314f5950342049553b3
Emma Barber <ecbarber1@gmail.com>
2013-07-06 21:19:57 -0400

    Added report()

--- command.js
+++ command.js
@@ -1,23 +1,36 @@
 // Game state functions
 var initGame = function(phoneNumber) {
-
+	//register user as player in db
+	//SET uid:phone_number:users
+	//SET uid:phone_number:competitors
+	//SET uid:phone_number:employees
+	//SET uid:phone_number:features
+	//SET uid:phone_number:investors
+	//SET uid:phone_number:money
 };
 var ipo = function(phoneNumber) {
-
+	//remove user from db
+	
 };
 
 // User information
-var help = function() {
-
+var help = function(phoneNumber) {
+	//return help manual
+	
 };
-var report = function() {
+var state = function(phoneNumber) {
+	//return
+};
+var report = function(phoneNumber) {
 
 };
 
 //User interaction
-var buy = function() {
-
+var buy = function(company) {
+	//lookup amount for company
+	//remove amount from current balance
 };
-var devProject = function() {
 
+var devProject = function(phoneNumber) {
+	//auto
 };
