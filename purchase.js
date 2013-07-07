166ab14e4f1d137f1abfc59934725f46f71be637
Lukas Schwab <lukas.schwab@gmail.com>
2013-07-07 01:20:02 -0700

    Added purchase.js

---
+++ purchase.js
@@ -0,0 +1,14 @@
+exports.purchase = function purchase(offer,competitor) {
+
+var users = ,
+rpu = ,
+emp = ,
+balance = ;
+
+if (offer > competitor_worth - tolerance) {
+	(users) = (users) + competitor['users'];
+	(rpu) = (rpu) + competitor['rpu'];
+	(emp) = (emp) + competitor['emp'];
+	(balance) = (balance) - offer
+}
+};
