12cd48ce6f395bfa933783d4a7e1dcf8179c8a7b
Emma Barber <ecbarber1@gmail.com>
2013-07-06 20:07:13 -0400

    Added framework for user commands

---
+++ command.js
@@ -0,0 +1,23 @@
+// Game state functions
+var initGame = function(phoneNumber) {
+
+};
+var ipo = function(phoneNumber) {
+
+};
+
+// User information
+var help = function() {
+
+};
+var report = function() {
+
+};
+
+//User interaction
+var buy = function() {
+
+};
+var devProject = function() {
+
+};
