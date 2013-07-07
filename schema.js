4f08ca36a670acb814cddee9a95a6b9221b2587e
Emma Barber <ecbarber1@gmail.com>
2013-07-07 04:33:50 -0400

    Added skeleton setter functions

--- schema.js
+++ schema.js
@@ -17,12 +17,12 @@ var User = mongoose.model('User', schema);
 var getGameState = function(phoneNumber) {
 	return User.find({phoneNumber: phoneNumber} , 'gamestate', function(err, user) {
 		return user.gamestate;
-	})
+	});
 }
 var getCurrentBalance = function(phoneNumber) {
 	return User.find({phoneNumber: phoneNumber}, 'currentBalance', function(err, user) {
 		return user.currentBalance;
-	})
+	});
 }
 var getUsers = function(phoneNumber) {
 	return User.find({phoneNumber: phoneNumber}, 'users', function(err, user) {
@@ -30,11 +30,36 @@ var getUsers = function(phoneNumber) {
 	});
 }
 var getEmployees = function(phoneNumber) {
-
+	return User.find({phoneNumber: phoneNumber}, 'employees', function(err, user) {
+		return user.employees;
+	});
 }
 var getAdvertisers = function(phoneNumber) {
-
+	return User.find({phoneNumber: phoneNumber}, 'advertisers', function(err, user) {
+		return user.advertisers;
+	});
 }
 var getCompetitors = function(phoneNumber) {
+	return User.find({phoneNumber: phoneNumber}, 'competitors', function(err, user) {
+		return user.competitors;
+	});
+}
+
+var setGameState = function(phoneNumber, state) {
+	
+}
+var setCurrentBalance = function(phoneNumber, newBalance) {
+
+}
+var setUsers = function(phoneNumber, newUsers) {
+
+}
+var setEmployees = function(phoneNumber, newEmployees) {
+
+}
+var setAdvertisers = function(phoneNumber, newAdvertisers) {
+
+}
+var setCompetitors = function(phoneNumber, newCompetitors) {
 
 }
