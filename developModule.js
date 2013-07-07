c751373e0e230a50dfaf4b9a48a85ab67f99a492
Lukas Schwab <lukas.schwab@gmail.com>
2013-07-07 07:37:48 -0700

    Updated to load to phone number

--- developModule.js
+++ developModule.js
@@ -5,10 +5,10 @@ db = require('./db');
 var player = db.getPlayer();
 	
 var rand = Math.random() * (max - min) + min
-db.set(users,(users) + rand*(users))
+db.set(player.phoneNumber,users,(users) + rand*(users))
 
 var rand = Math.random() * (max - min) + min
-db.set(rpu, (rpu) + rand*(rpu))
+db.set(player.phoneNumber,rpu, (rpu) + rand*(rpu))
 };
 
-db.set(employees, employees + 10)
+db.set(player.phoneNumber,employees, employees + 10)
--- purchase.js
+++ purchase.js
@@ -8,10 +8,12 @@ var competitor = require('./competitorGeneration')
 // Does the first element (the property) have to be a string to redefine it?
 
 if (offer > competitor.worth - tolerance) {
-	db.set(users,(users) + competitor.users);
-	db.set(rpu, (rpu)*((users)/(users + competitor.users)) + competitor.rpu*((competitor.users)/(users + competitor.users)));
-	db.set(employees, (emp) + competitor.emp);
-	db.set(balance, (balance) - offer);
+	db.set(player.phoneNumber,users,(users) + competitor.users);
+	db.set(player.phoneNumber,rpu, (rpu)*((users)/(users + competitor.users)) + competitor.rpu*((competitor.users)/(users + competitor.users)));
+	db.set(player.phoneNumber,employees, (emp) + competitor.emp);
+	db.set(player.phoneNumber,balance, (balance) - offer);
 }
-db.send()
 };
+
+
+// +15037296270
