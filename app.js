9c3c0a60007a37a3c16a471cbe86d7c550231b0a
Hamzah Khan <khanh@catlin.edu>
2013-07-06 21:08:42 -0700

    Remove redis dependencies
    Will add mongo in future

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//hi
+
 /**
  * Module dependencies.
  */
@@ -7,14 +7,12 @@ var express = require('express'),
   http = require('http'),
   path = require('path'),
   stylus = require('stylus'),
-  redis = require('redis'),
   CONFIG = require('./CONFIG.json');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('redisPort', 6379);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -24,20 +22,6 @@ app.configure(function(){
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
-});
-
-var redis_cli = redis.createClient(6379,'127.0.0.1');
-
-redis_cli.on('connect',function() {
-  console.log('connected!');
-  redis_cli.auth(CONFIG.redispass);
-  console.log('authorized!');
-  redis_cli.set('test','test');
-  console.log(redis_cli.get('test'));
-});
-
-redis_cli.on('error',function() {
-  console.error('ERROR!');
 });
 
 app.configure('development', function(){
