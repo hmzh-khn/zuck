8d7196d626e6e2bad6931e4773a6ff9594ec3f2b
Lukas Schwab <lukas.schwab@gmail.com>
2013-07-06 23:24:08 -0700

    Added module to execute the changes under "develop" Still needs to be modified to actually call and write objects from the database

---
+++ developModule.js
@@ -0,0 +1,8 @@
+exports.rand = function modify(min1,max1,min2,max2) {
+	
+var rand = Math.random() * (max - min) + min
+var (users) = (users) + rand*(users)
+
+var rand = Math.random() * (max - min) + min
+var (rpu) = (rpu) + rand
+};
