81924d9b3a75f15e3bcfd6a1dc3f3818da19fc45
Hamzah Khan <khanh@catlin.edu>
2013-08-13 16:32:14 -0700

    change default port number from 3000 => 8000

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 global.DATA = {};
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
